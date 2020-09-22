c409fcf88f2ff597ee9af41a255a2f62f0850377
Valeriu-Nicolas Vancea <valeriuvancea@gmail.com>
2020-09-23 00:07:02 +0200

    Add a title to the error page

--- src/app.ts
+++ src/app.ts
@@ -38,7 +38,7 @@ app.use(function(err: any, req: any, res: any, next: any) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {title: "404 - Page not found"});
 });
 
 module.exports = app;
