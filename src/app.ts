525288c6026877f9413c7c964759446e7b4bd70c
flamenaak <vlada.rocin@gmail.com>
2020-09-23 14:36:22 +0200

    Seting up connection to mongoDb atlas
    there is a example model created for user and users/test endpoint to show how to query it

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var registerRouter = require('./routes/register');
 
+import mongoose from 'mongoose'
+
 var app = express();
 
 app.engine('ejs', require('ejs').renderFile);
@@ -22,23 +24,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/login',loginRouter);
-app.use('/register',registerRouter)
+app.use('/login', loginRouter);
+app.use('/register', registerRouter)
+
+//setup mongo connection and mongoose
+const uri = "mongodb+srv://administrator:secret2.@cluster0.9buds.gcp.mongodb.net/fitnessApp?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("connected to mongo");
+  })
+  .catch(err => {
+    console.error("problem connecting to mongo", err);
+  });
 
 // catch 404 and forward to error handler
-app.use(function(req: any, res: any, next: any) {
+app.use(function (req: any, res: any, next: any) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err: any, req: any, res: any, next: any) {
+app.use(function (err: any, req: any, res: any, next: any) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', {title: "404 - Page not found"});
+  res.render('error', { title: "404 - Page not found" });
 });
+
 
 module.exports = app;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,25 @@
+import User from '../model/User'
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req: any, res: any, next: any) {
+router.get('/', function (req: any, res: any, next: any) {
   res.send('respond with a resource');
 });
+
+/* GET users listing. */
+router.get('/test', async function (req: any, res: any, next: any) {
+  let count = await User.countDocuments({ userName: "name" });
+      if (count === 0) {
+        let user = new User({ userName: "name", password: "pass" });
+        user.save();
+      } else {
+        res.send("User count: " + count);
+        return;
+      }
+  count = await User.countDocuments({ userName: "name" });
+  res.send("User count: " + count);
+ });
 
 module.exports = router;
